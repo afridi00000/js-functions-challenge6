ec67713543d08ed8caadb4c6e5a83e12d92110ce
afridi00000 <afridi@usf.edu>
2025-02-05 20:10:38 -0500

    Task 3 - Employee Bonus Calculation.

--- cc_6.js
+++ cc_6.js
@@ -18,3 +18,27 @@ const calculateSalesTax = function(amount, taxRate) {
 
 calculateSalesTax(100, 0.07); 
 calculateSalesTax(500, 0.1); 
+// Task 3: Arrow Function
+const calculateBonus = (salary, performanceRating) => {
+    let bonusPercentage;
+    switch (performanceRating) {
+        case "Excellent":
+            bonusPercentage = 0.2;
+            break;
+        case "Good":
+            bonusPercentage = 0.1;
+            break;
+        case "Average":
+            bonusPercentage = 0.05;
+            break;
+        default:
+            bonusPercentage = 0;
+    }
+    const bonus = salary * bonusPercentage;
+    console.log(`Bonus: $${bonus}`);
+    return bonus;
+};
+
+
+calculateBonus(5000, "Excellent"); 
+calculateBonus(7000, "Good");      
