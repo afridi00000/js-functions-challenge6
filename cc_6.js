8b3bfa71a33d85db780d4e7ffffa871b5f03a3eb
afridi00000 <afridi@usf.edu>
2025-02-05 20:10:08 -0500

    Task 2 - Sales Tax Computation.

--- cc_6.js
+++ cc_6.js
@@ -8,3 +8,13 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
 
 calculateProfit(20, 30, 100); 
 calculateProfit(50, 70, 200); 
+// Task 2: Function Expression
+const calculateSalesTax = function(amount, taxRate) {
+    const salesTax = amount * taxRate;
+    console.log(`Sales Tax: $${salesTax}`);
+    return salesTax;
+};
+
+
+calculateSalesTax(100, 0.07); 
+calculateSalesTax(500, 0.1); 
